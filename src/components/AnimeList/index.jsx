7b3ecc44b1a49d651fe16ee4aabd8d8201f38e5f
Rian Priansyah <rian.priansyah04@gmail.com>
2024-05-14 14:29:25 +0700

    vide player & reusable api call

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -7,8 +7,8 @@ const AnimeList = ({ api }) => {
       {api.data?.map((anime, index) => {
         return (
           // bug in id
-          <Link href={`/${anime.mal_id}`} className="cursor-pointer text-color-primary hover:text-color-accent transition-all" key={index}>
-            <Image src={anime.images.webp.image_url} width={200} height={350} alt="..." className="w-full max-h-64 object-cover" />
+          <Link href={`/anime/${anime.mal_id}`} className="cursor-pointer text-color-primary hover:text-color-accent transition-all" key={index}>
+            <Image src={anime.images.webp.image_url} width={350} height={350} alt="..." className="w-full max-h-64 object-cover" />
             <h2 className="font-bold md:text-xl  text-md p-4 text-center ">{anime.title}</h2>
           </Link>
         );
