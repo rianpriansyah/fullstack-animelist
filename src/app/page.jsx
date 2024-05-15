5abf3735e0db7c6b3baaec66a7049235efd07114
Rian Priansyah <rian.priansyah04@gmail.com>
2024-05-15 20:54:50 +0700

    random data dengan logiak

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 import AnimeList from "../components/AnimeList";
 import Header from "@/components/AnimeList/Header";
-import { getAnimeResponse, getNestedAnimeResponse } from "@/libs/api-libs";
+import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "@/libs/api-libs";
 
 const Page = async () => {
   const topAnime = await getAnimeResponse("top/anime", "limit=8");
   let recomendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry");
-  recomendedAnime = { data: recomendedAnime.slice(0, 4) };
+  recomendedAnime = reproduce(recomendedAnime, 4);
 
   return (
     <div className="px-4">
