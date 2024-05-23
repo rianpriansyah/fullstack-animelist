3e993b018ce3b9588515bf572dc356b0b4c83014
Rian Priansyah <rian.priansyah04@gmail.com>
2024-05-23 13:51:50 +0700

    validasi user dasboard

--- src/app/user/dasboard/page.jsx
+++ src/app/user/dasboard/page.jsx
@@ -1,13 +1,22 @@
 import { authUserSession } from "@/libs/auth-libs";
 import Image from "next/image";
+import Link from "next/link";
 
 const Page = async () => {
   const user = await authUserSession();
+
   return (
-    <div className="text-color-primary">
-      <h3>DASBOARD</h3>
-      <h5>Welcome, {user.name}</h5>
+    <div className="mt-8 flex flex-col justify-center items-center text-color-primary">
+      <h5 className="text-2xl font-bold">Welcome, {user.name}</h5>
       <Image src={user.image} alt="..." width={250} height={250} />
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link href={"/user/dasboard/collection"} className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl">
+          My Collection
+        </Link>
+        <Link href={"/user/dasboard/comment"} className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl">
+          My Comment
+        </Link>
+      </div>
     </div>
   );
 };
